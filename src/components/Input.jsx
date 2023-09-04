c1fbea8824f5826a1c4dd1ead84cd7d5db158523
root <root@DESKTOP-NQDTF3U>
2023-09-03 21:20:04 -0700

    styling sidebar

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Input({ handleChange, value, title, collapsed }) {
   return (
-    <button className={`flex items-center text-sm font-medium p-2 hover:bg-white/100 rounded-md w-full mt-2 pl-5 ${collapsed && 'opacity-0 translate-x-28'}`} onClick={handleChange} value={value}>
+    <button className={`flex items-center p-2 btn shadow-[0_3px_0_rgb(0,0,0)] text-black hover:text-white hover:bg-black focus:text-white focus:bg-black rounded-lg w-full mt-2 pl-5 ${collapsed && 'opacity-0 translate-x-28'}`} onClick={handleChange} value={value}>
       {title}
     </button>
   );
--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -8,21 +8,46 @@ import { AiOutlineLeft } from 'react-icons/ai';
 
 function Sidebar({ handleChange }) {
     const [collapsed, setSidebarCollapsed] = useState(false);
-    return <>
-        {/* SIDEBAR */}
-        <section className="absolute">
-            {/*//@ts-ignore*/}
-            <div className={`flex flex-col px-5 rounded backdrop-blur-xl bg-white/50 min-h-screen ${!collapsed ? 'w-60' : 'w-16'} duration-500`}>
-                <div className='py-3 flex justify-end'>
-                    <AiOutlineLeft size={26} className='cursor-pointer' onClick={() => setSidebarCollapsed(!collapsed)} />
-                </div>
-                <img src={logo} className={`justify-center h-[150px] w-[150px] mb-5 rounded-[46%] bg-black ${collapsed && 'opacity-0 translate-x-28'}`} alt="vannityHMO" />
+
+    const sidebarStyles = {
+        filter: 'drop-shadow(10px 0 10px #00000030)'
+    };
+
+    return  <>
+    <div
+          x-transition:enter="transform transition-transform duration-300"
+          x-transition:enter-start="-translate-x-full"
+          x-transition:enter-end="translate-x-0"
+          x-transition:leave="transform transition-transform duration-300"
+          x-transition:leave-start="translate-x-0"
+          x-transition:leave-end="-translate-x-full"
+          class="fixed inset-y-0 z-10 flex w-80"
+        >
+          <svg
+            class={`flex flex-col text-white/50 h-full backdrop-blur-xl  ${!collapsed ? 'w-80' : 'w-16'} duration-500`}
+            style={sidebarStyles}
+            preserveAspectRatio="none"
+            viewBox="0 0 309 800"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M268.487 0H0V800H247.32C207.957 725 207.975 492.294 268.487 367.647C329 243 314.906 53.4314 268.487 0Z"
+              
+            />
+          </svg>  
+         
+         <div className='absolute grid-col px-12'>
+         <div className='py-5 flex justify-end'>
+                <AiOutlineLeft size={26} className='cursor-pointer' onClick={() => setSidebarCollapsed(!collapsed)} />
+            </div>
+                <img src={logo} className={`h-[150px] w-[150px] mb-5 rounded-[46%] bg-black ${collapsed && 'opacity-0 translate-x-28'}`} alt="vannityHMO" />
                 <Category handleChange={handleChange} collapsed={collapsed} />
                 <Price handleChange={handleChange} collapsed={collapsed} />
                 <Color handleChange={handleChange} collapsed={collapsed} />
             </div>
-        </section >
-    </>
+            </div>
+     </>
 }
 
 export default Sidebar
