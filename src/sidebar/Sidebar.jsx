33acb11c0a6e86a9120b5e5f1622e476c9108270
root <root@DESKTOP-NQDTF3U>
2023-09-08 05:02:18 -0700

    changing sidebar shape

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -10,12 +10,14 @@ import useWindowDimensions from '../hooks/useWindowDimension';
 
 function Sidebar({ handleChange }) {
   const [shown, setIsShown] = useState(false)
-
+  console.log(shown)
   const handleOnChange = () => {
     setIsShown(prev => !prev)
   }
 
   const { width, height } = useWindowDimensions()
+
+  console.log(width, height)
 
   const sidebarStyles = {
     filter: 'drop-shadow(10px 0 10px #00000030)'
@@ -23,30 +25,17 @@ function Sidebar({ handleChange }) {
 
   return <>
     <div
-      style={{ width: !shown && width == "498" ? "0px" : "300px"}}
-      class="fixed inset-y-0 flex duration-500"
-      handleOnChange={handleOnChange}
+      style={{ width: !shown && width == 360 ? 0 : "300px" }}
+      class={`flex flex-col rounded-lg fixed w-64 h-full px-4 py-8 overflow-y-auto bg-white/50 ${!shown ? "w-0" : 'w-80'} transform-500`}
+      shown={shown}
     >
-      <svg
-        class='flex flex-col text-white/50 h-full backdrop-blur-xl'
-        style={sidebarStyles}
-        preserveAspectRatio="none"
-        viewBox="0 0 309 800"
-        fill="currentColor"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M268.487 0H0V800H247.32C207.957 725 207.975 492.294 268.487 367.647C329 243 314.906 53.4314 268.487 0Z"
-
-        />
-      </svg>
-      <div style={{ visibility: !shown && height == "311" ? "hidden" : "visible"}} className='absolute items-center font-medium'>
-        <img src={logo} className='h-[150px] w-[150px] mb-5 rounded-[46%] bg-black' alt="vannityHMO" />
+      <div style={{ visibility: !shown && height == "640" ? "hidden" : "visible" }} className='flex flex-col gap-1 pt-10 ml-10 font-medium'>
+        <img src={logo} className='mb-16 h-[170px] w-[170px] rounded-[46%] bg-black' alt="vannityHMO" />
         <Category handleChange={handleChange} shown={shown} />
         <Price handleChange={handleChange} shown={shown} />
         <Color handleChange={handleChange} shown={shown} />
       </div>
-    </div >
+    </div>
     <button className={`fixed lg:hidden bg-white/50 z-90 justify-center items-center bottom-10 right-10 w-10 h-10 rounded-full drop-shadow-lg flex duration-500`} onClick={handleOnChange}>
       <FcLike size={26} className='cursor-pointer' />
     </button>
